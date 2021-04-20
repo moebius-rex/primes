fbf5f02ce1db6b5ca426fc705a45419a960778dd
Shay Gordon <moebius.le.roi@gmail.com>
2021-04-20 17:25:33 -0400

    Build sieve .a and .so and run test apps
    Also find optimal methods for configuring loader on each linux system.

--- installer/install.sh
+++ installer/install.sh
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#		 http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,7 +27,7 @@ make clean
 # make sieve libraries
 cd ${projpath}/src/main/c/sieve
 libpath=$(pwd)/lib
-make targets lib/libcsieve.so
+make targets lib/libcsieve.a lib/libcsieve.so
 
 # Three ways shared libraries may be located by loader in order of permanence:
 # 1. libraries are described in /etc/ld.so.conf (not on all systems)
@@ -36,36 +36,48 @@ make targets lib/libcsieve.so
 
 # if no /usr/local/lib, use LD_LIBRARY_PATH to identify shared objects
 if [ ! -d /usr/local/lib ]; then
-  rc=".$(basename ${SHELL})rc"
-  echo "-----------------------------------------------------------------------------"
-  echo "                              I M P O R T A N T                              "
-  echo "-----------------------------------------------------------------------------"
-  echo "Add the following line to the end of the ${rc} file in your home directory:"
-  echo 
-  echo "export LD_LIBRARY_PATH=${libpath}"
-  echo
-  echo "Then, source the modified file to apply the change:"
-  echo "  % source \$HOME/${rc}"
-  echo
-  echo "To verify the change:"
-  echo "  % env|grep LD_LIBRARY_PATH"
-  echo "  LD_LIBRARY_PATH=${libpath}"
-  echo
-  echo "Alternatively, log out and back in for the change to take effect"
-  echo "-----------------------------------------------------------------------------"
+	rc=".$(basename ${SHELL})rc"
+	echo "-----------------------------------------------------------------------------"
+	echo "                              I M P O R T A N T															"
+	echo "-----------------------------------------------------------------------------"
+	echo "Add the following line to the end of the ${rc} file in your home directory:"
+	echo 
+	echo "export LD_LIBRARY_PATH=${libpath}"
+	echo
+	echo "Then, source the modified file to apply the change:"
+	echo "	% source \$HOME/${rc}"
+	echo
+	echo "To verify the change:"
+	echo "	% env|grep LD_LIBRARY_PATH"
+	echo "	LD_LIBRARY_PATH=${libpath}"
+	echo
+	echo "Alternatively, log out and back in for the change to take effect"
+	echo "-----------------------------------------------------------------------------"
 fi
 
 # create softlink to sieve shared library in /usr/local/lib
 target=/usr/local/lib/libcsieve.so
 if [ -L ${target} ]; then
-  unlink ${target}
+	unlink ${target}
 fi
 ln -s ${libpath}/libcsieve.so ${target}
 
-# configure sieve shared library in loader if rquired
-confdir=/etc/ld.so.conf.d
-if [ -d ${confdir} ]; then
-  cp ${projpath}/installer/libcsieve.so.conf ${confdir}/libcsieve.conf
-  ldconfig
-  # ldconfig -n ${projpath}/src/main/c/sieve
+# add sieve shared library to loader config if required
+if [ -x $(which ldconfig) ]; then
+	confdir=/etc/ld.so.conf.d
+	if [ -d ${confdir} ]; then
+		echo cp ${projpath}/installer/libcsieve.so.conf ${confdir}
+		cp ${projpath}/installer/libcsieve.so.conf ${confdir}
+	else
+		# some systems, like alpine, don't use a config directory
+		ldconfig -n $(dirname ${target})
+	fi
+	ldconfig
+  if [ $? != 0 ]; then
+    echo error: ldconfig exited with non-zero status $?
+  fi
 fi
+
+# run sieve library test app
+cd ${projpath}/src/main/c/sieve
+make test
