c2d82ad86904f0ccc80f4f8668d926211bcdf477
Shay Gordon <moebius.le.roi@gmail.com>
2021-04-20 11:01:37 -0400

    Minor cleaning

--- installer/install.sh
+++ installer/install.sh
@@ -14,21 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# get the normalized path of this script, i.e., the project home directory
-projpath=$(pwd)/$(dirname $0)
-projpath=$(cd ${projpath}/../ && pwd)
+# get the normalized path of this script's parent directory, i.e., the
+# project home directory
+projpath=$(pwd)/$(dirname $0)     # current directory
+projpath=$(cd ${projpath} && pwd) # normalized current directory
+projpath=$(dirname ${projpath})   # parent directory
 
 # clean all generated project files
 cd ${projpath}
 make clean
 
-# make sieved libraries
+# make sieve libraries
 cd ${projpath}/src/main/c/sieve
 libpath=$(pwd)/lib
 make targets lib/libcsieve.so
 
 # Three ways shared libraries may be located by loader in order of permanence:
-# 1. libraries are described in /etc/ld.so.conf
+# 1. libraries are described in /etc/ld.so.conf (not on all systems)
 # 2. libraries are searched for in /usr/local/lib
 # 3. libraries are searched for in LD_LIBRARY_PATH environment variable
 
@@ -42,26 +44,28 @@ if [ ! -d /usr/local/lib ]; then
   echo 
   echo "export LD_LIBRARY_PATH=${libpath}"
   echo
-  echo "Then source the modified file to apply the change:"
-  echo "% source ${rc}"
-  echo "% env|grep LD_LIBRARY_PATH"
-  echo "LD_LIBRARY_PATH=${libpath}"
+  echo "Then, source the modified file to apply the change:"
+  echo "  % source \$HOME/${rc}"
+  echo
+  echo "To verify the change:"
+  echo "  % env|grep LD_LIBRARY_PATH"
+  echo "  LD_LIBRARY_PATH=${libpath}"
   echo
   echo "Alternatively, log out and back in for the change to take effect"
   echo "-----------------------------------------------------------------------------"
-  exit
 fi
 
-# install softlink in /usr/local/lib
+# create softlink to sieve shared library in /usr/local/lib
 target=/usr/local/lib/libcsieve.so
 if [ -L ${target} ]; then
   unlink ${target}
 fi
 ln -s ${libpath}/libcsieve.so ${target}
 
-# configure libcsieve.so in loader if rquired
-target=/etc/ld.so.conf.d
-if [ -d ${target} ]; then
-  cp ${projpath}/installer/libcsieve.so.conf ${target}
+# configure sieve shared library in loader if rquired
+confdir=/etc/ld.so.conf.d
+if [ -d ${confdir} ]; then
+  cp ${projpath}/installer/libcsieve.so.conf ${confdir}/libcsieve.conf
   ldconfig
+  # ldconfig -n ${projpath}/src/main/c/sieve
 fi
