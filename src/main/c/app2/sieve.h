0b8cab9f7c6e7190122e71cfaac5f3c74802df69
Shay Gordon <moebius.le.roi@gmail.com>
2021-04-21 17:54:17 -0400

    Remove blank line before pragma

--- src/main/c/app2/sieve.h
+++ src/main/c/app2/sieve.h
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 #pragma once
 
 #include <stdbool.h>
--- src/main/cpp/app3/sieve.h
+++ src/main/cpp/app3/sieve.h
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 #pragma once
 
 #include <sys/time.h>
